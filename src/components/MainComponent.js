1e321a349746e878285d0f7007a157646f0a2860
SuriyaaVijay <suriyaa2002@gmail.com>
2023-01-31 20:59:59 +0530

    SPA1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 
@@ -13,21 +16,28 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
-    const HomePage = () => {
-      return (
-        <Home
-        />
-      );
-    }
 
     const MenU = () => {
      return(
       <Menu dishes = {this.state.dishes} />
      ); 
+    }
+
+    const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
     }
 
     return (
@@ -37,6 +47,7 @@ class Main extends Component {
         
           <Route path='/home' exact element={<HomePage/>} />
           <Route path='/menu' exact element={<MenU/>} />
+          <Route path='/contactus' exact element={<Contact/>}/>
           <Route path='/home' element={<Navigate replace to="/home" /> } /> 
           
         </Routes>
